733324a7d77ec575b36301a3f1f6154a90373d5b
Victor Silva <silvavictorleandro@outlook.com>
2024-04-06 15:42:47 -0300

    correção espaçamento card list

--- src/components/ProductList/styles.ts
+++ src/components/ProductList/styles.ts
@@ -10,10 +10,10 @@ export const Main = styled.main`
 `;
 
 export const Container = styled.div<ShowCartProps>`
+  align-content: flex-start;
   display: flex;
   flex-wrap: wrap;
   gap: 2rem;
-  justify-content: center;
   margin: 3rem auto;
   max-width: 900px;
 
